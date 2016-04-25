9bdf455d8c60fb3e805beb6601e1b9cf50bef055
Stephen Mayeux <stephenmayeux@gmail.com>
2016-04-26 01:23:52 +0900

    tag views

--- app/models/article.rb
+++ app/models/article.rb
@@ -6,7 +6,8 @@ class Article < ActiveRecord::Base
   def tag_list
     self.tags.collect do |tag|
       tag.name
-  end.join(", ")
+    end.join(", ")
+  end
 
   def tag_list=(tags_string)
     tag_names = tags_string.split(",").collect{ |s| s.strip.downcase }.uniq
